7916319adee55fce32ef1c8fb1a7bd1bc9286526
CptJackFrost <cptjackfrost74@gmail.com>
2020-07-16 20:48:36 +0500

    выбор региона пофикшен

--- src/main/java/Avito.java
+++ src/main/java/Avito.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,24 +17,19 @@ public class Avito {
         driver.get("https://www.avito.ru/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#category option[value='99']")));
         driver.findElement(By.cssSelector("#category option[value='99']")).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#search")));
         driver.findElement(By.cssSelector("#search")).sendKeys("Принтер");
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-marker='search-form/region']")));
         driver.findElement(By.cssSelector("div[data-marker='search-form/region']")).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[data-marker='popup-location/region/input']")));
         driver.findElement(By.cssSelector("input[data-marker='popup-location/region/input']"))
                 .sendKeys("Владивосток");
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li[data-marker='suggest(0)']")));
+        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.cssSelector("li[data-marker='suggest(0)']")), "Владивосток"));
         driver.findElement(By.cssSelector("*[data-marker='suggest(0)']")).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-marker='popup-location/save-button']")));
         driver.findElement(By.cssSelector("button[data-marker='popup-location/save-button']")).click();
+
 
 
         //WebElement params = driver.findElement(By.cssSelector("._1dp226jcDt_jum2k1IGKC_"));
